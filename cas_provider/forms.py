51b7a9aeebc3d20249ab44d95b32e770ce92c957
bxf_100_u <baixuexue123@hotmail.com>
2016-07-19 18:52:25 +0800

    add_qs

--- cas_provider/forms.py
+++ cas_provider/forms.py
@@ -7,7 +7,6 @@ from .utils import create_login_ticket
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=32)
     password = forms.CharField(widget=forms.PasswordInput)
-    # warn = forms.BooleanField(required=False)  # TODO: Implement
     lt = forms.CharField(widget=forms.HiddenInput, initial=create_login_ticket)
 
     def __init__(self, service=None, renew=None, gateway=None, request=None, *args, **kwargs):
--- cas_provider/views.py
+++ cas_provider/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.conf import settings
+from django.contrib import messages
 
 from .utils import add_qs, create_service_ticket
 from .forms import LoginForm
@@ -17,7 +18,7 @@ def login(request, template_name='cas/login.html', success_redirect=None):
     if success_redirect is None:
         success_redirect = settings.LOGIN_REDIRECT_URL or '/accounts/profile/'
 
-    service = request.GET.get('service', None)
+    service = request.GET.get('service')
     if request.user.is_authenticated():
         if service is not None:
             ticket = create_service_ticket(request.user, service)
@@ -25,21 +26,20 @@ def login(request, template_name='cas/login.html', success_redirect=None):
         else:
             return HttpResponseRedirect(success_redirect)
 
-    errors = []
     if request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        service = request.POST.get('service', None)
-        lt = request.POST.get('lt', None)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        service = request.POST.get('service')
+        lt = request.POST.get('lt')
 
         try:
             login_ticket = LoginTicket.objects.get(ticket=lt)
         except LoginTicket.DoesNotExist:
-            errors.append('Login ticket expired. Please try again.')
+            messages.add_message(request, messages.INFO, 'Login ticket expired. Please try again.')
         else:
             login_ticket.delete()
             user = authenticate(username=username, password=password)
-            if user is not None:
+            if user:
                 if user.is_active:
                     auth_login(request, user)
                     if service is not None:
@@ -48,17 +48,17 @@ def login(request, template_name='cas/login.html', success_redirect=None):
                     else:
                         return HttpResponseRedirect(success_redirect)
                 else:
-                    errors.append('This account is disabled.')
+                    messages.add_message(request, messages.INFO, 'This account is disabled.')
             else:
-                errors.append('Incorrect username and/or password.')
+                messages.add_message(request, messages.INFO, 'Incorrect username and/or password.')
+
     form = LoginForm(service)
-    return render_to_response(template_name, {'form': form, 'errors': errors},
-                              context_instance=RequestContext(request))
+    return render_to_response(template_name, {'form': form}, context_instance=RequestContext(request))
 
 
 def validate(request):
-    service = request.GET.get('service', None)
-    ticket_string = request.GET.get('ticket', None)
+    service = request.GET.get('service')
+    ticket_string = request.GET.get('ticket')
     if service is not None and ticket_string is not None:
         try:
             ticket = ServiceTicket.objects.get(ticket=ticket_string)
@@ -72,6 +72,6 @@ def validate(request):
 
 
 def logout(request, template_name='cas/logout.html'):
-    url = request.GET.get('url', None)
+    url = request.GET.get('url')
     auth_logout(request)
     return render_to_response(template_name, {'url': url}, context_instance=RequestContext(request))
