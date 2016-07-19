1e7232bcf6d79f0f3b9bbaf9354c3032625f6d9d
bxf_100_u <baixuexue123@hotmail.com>
2016-07-19 19:48:06 +0800

    get_random_string

--- cas_provider/utils.py
+++ cas_provider/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import string
 import urllib
-from random import Random
+
+from django.utils.crypto import get_random_string
 
 from .models import ServiceTicket, LoginTicket
 
@@ -16,19 +16,11 @@ def add_qs(url, **kwargs):
         return url+'&'+qs
 
 
-def _generate_string(length=8, chars=string.ascii_letters + string.digits):
-    """
-    Generates a random string of the requested length. Used for creation of tickets.
-    """
-    return ''.join(Random().sample(chars, length))
-
-
 def create_service_ticket(user, service):
     """
     Creates a new service ticket for the specified user and service.
-    Uses _generate_string.
     """
-    ticket_string = 'ST-' + _generate_string(29)  # Total ticket length = 29 + 3 = 32
+    ticket_string = 'ST-' + get_random_string(length=29)  # Total ticket length = 29 + 3 = 32
     ticket = ServiceTicket(service=service, user=user, ticket=ticket_string)
     ticket.save()
     return ticket
@@ -38,7 +30,7 @@ def create_login_ticket():
     """
     Creates a new login ticket for the login form. Uses _generate_string.
     """
-    ticket_string = 'LT-' + _generate_string(29)
+    ticket_string = 'LT-' + get_random_string(length=29)
     ticket = LoginTicket(ticket=ticket_string)
     ticket.save()
     return ticket_string
